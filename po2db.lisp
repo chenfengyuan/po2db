8ea08ad22628d3c7a9c3c52887586f9a488cc81e
Fengyuan Chen <cfy1990@gmail.com>
2012-01-31 09:55:44 +0800

    auto hot upgrade,example : ./po2db po2db.lisp --args-as-before-version--

--- po2db.lisp
+++ po2db.lisp
@@ -26,7 +26,21 @@
 (defvar *default-table-suffix* "default")
 (defvar *default-table-prefix* "t_")
 (defvar *default-headinfo-prefix* "h_")
-
+(defun concatenate-strings(&rest strings)
+  (apply #'concatenate 'string strings))
+(defparameter *version* (let* ((arg (cadr #+sbcl sb-ext:*posix-argv*))
+			       (file (or (and
+					  arg
+					  (scan "\\.lisp$" arg)
+					  (probe-file arg))
+					 (probe-file "po2db.lisp"))))
+			  ( if file 
+			       (file-write-date file)
+			       0)))
+(defparameter *version-string*
+  (if (plusp *version*)
+      (format nil "(~a)" *version*)
+      ""))
 ;;file coding(utf-8)
 #+ccl
 (setf ccl:*default-external-format* :utf-8)
@@ -37,8 +51,8 @@
 
 (defvar mydebug nil)
 (defun mydebug(&rest rest)
-    (if mydebug
-	(apply #'format rest)))
+  (if mydebug
+      (apply #'format rest)))
 (defun flatlist (l)
   (cond
     ((null l) nil)
@@ -62,8 +76,6 @@
 	      count (search "msgid" i) into id
 	      finally (return (list s id))))))
 
-(defun concatenate-strings(&rest strings)
-  (apply #'concatenate 'string strings))
 
 (let ((po)(index)(total))
   (defun po-clear()
@@ -181,54 +193,54 @@
 
 (defun  po-parse()
   (let* ((id)(str)(ctxt)(flag)(result (make-array 0 :fill-pointer t :adjustable t))
-	(when-id (lambda (new-id)
-		   (if (eql nil id)
-		       (setf id new-id)
-		       (error (format nil "dumplicated id:~a~%" (po-index))))))
-	(when-str (lambda (new-str)
-		    (if (and (not (eql nil id)) (eql nil str))
-			(setf str new-str)
-			(error (format nil "error str:~a~%" (po-index))))))
-	(when-ctxt (lambda (new-ctxt)
-		     (if (eql nil ctxt)
-			 (setf ctxt new-ctxt)
-			 (error (format nil "dumplicated ctxt:~a~%" (po-index))))))
-	(when-flag (lambda (new-flag)
-		     (if (eql nil flag)
-			 (setf flag new-flag)
-			 (error (format nil "dumplicated flag:~a~%" (po-index))))))
-	(when-comment (lambda (string)
-			string
-			(cond ((and id str)
-			       (vector-push-extend (list id str ctxt flag) result)
-			       (setf id nil str nil ctxt nil flag nil))
-			      ((not (eql nil flag))
-			       (setf flag nil)))))
-	(when-blank-or-eof (lambda (string)
-			     string
-			     (mydebug t "blank:~a " (po-index))
-			     (cond ((and id str)
-				    (vector-push-extend (list id str ctxt flag) result)
-				    (setf id nil str nil ctxt nil flag nil)))))
-	(s0
-	 (lambda (s1 s2 fn &optional (ext-fun nil))
-	   (cond ((eql 0 (search s1 s2))
-		  (if (eql nil  ext-fun)
-		      (funcall fn s2)
-		      (funcall fn (funcall ext-fun)))
-		  t)
-		 (t nil))))
-	  (determined-when
-	   (lambda (string)
-	     (cond ((funcall s0 "msgid " string when-id #'po-read-whole-item-for-loop)(mydebug t "id:~a~%" (po-index)))
-		   ((funcall s0 "msgstr " string when-str #'po-read-whole-item-for-loop)(mydebug t "str:~a~%" (po-index)))
-		   ((funcall s0 "msgstr[0]" string when-str #'po-read-whole-item-for-loop)(mydebug t "str:~a~%" (po-index)))
-		   ((funcall s0 "msgctxt" string when-ctxt #'po-read-whole-item-for-loop)(mydebug t "ctxt:~a~%" (po-index)))
-		   ((funcall s0 "#," string when-flag)(mydebug t "#,:~a~%" (po-index)))
-		   ((eql nil string) (funcall s0 "" string when-blank-or-eof)(mydebug t "nil:~a~%" (po-index)))
-		   ((funcall s0 "#" string when-comment)(mydebug t "comment:~a~%" (po-index)))
-		   ((funcall s0 "" string when-blank-or-eof)(mydebug t "empty:~a~%" (po-index)))
-		   (t (error (format nil "unexpect:~a~%" string)))))))
+	 (when-id (lambda (new-id)
+		    (if (eql nil id)
+			(setf id new-id)
+			(error (format nil "dumplicated id:~a~%" (po-index))))))
+	 (when-str (lambda (new-str)
+		     (if (and (not (eql nil id)) (eql nil str))
+			 (setf str new-str)
+			 (error (format nil "error str:~a~%" (po-index))))))
+	 (when-ctxt (lambda (new-ctxt)
+		      (if (eql nil ctxt)
+			  (setf ctxt new-ctxt)
+			  (error (format nil "dumplicated ctxt:~a~%" (po-index))))))
+	 (when-flag (lambda (new-flag)
+		      (if (eql nil flag)
+			  (setf flag new-flag)
+			  (error (format nil "dumplicated flag:~a~%" (po-index))))))
+	 (when-comment (lambda (string)
+			 string
+			 (cond ((and id str)
+				(vector-push-extend (list id str ctxt flag) result)
+				(setf id nil str nil ctxt nil flag nil))
+			       ((not (eql nil flag))
+				(setf flag nil)))))
+	 (when-blank-or-eof (lambda (string)
+			      string
+			      (mydebug t "blank:~a " (po-index))
+			      (cond ((and id str)
+				     (vector-push-extend (list id str ctxt flag) result)
+				     (setf id nil str nil ctxt nil flag nil)))))
+	 (s0
+	  (lambda (s1 s2 fn &optional (ext-fun nil))
+	    (cond ((eql 0 (search s1 s2))
+		   (if (eql nil  ext-fun)
+		       (funcall fn s2)
+		       (funcall fn (funcall ext-fun)))
+		   t)
+		  (t nil))))
+	 (determined-when
+	  (lambda (string)
+	    (cond ((funcall s0 "msgid " string when-id #'po-read-whole-item-for-loop)(mydebug t "id:~a~%" (po-index)))
+		  ((funcall s0 "msgstr " string when-str #'po-read-whole-item-for-loop)(mydebug t "str:~a~%" (po-index)))
+		  ((funcall s0 "msgstr[0]" string when-str #'po-read-whole-item-for-loop)(mydebug t "str:~a~%" (po-index)))
+		  ((funcall s0 "msgctxt" string when-ctxt #'po-read-whole-item-for-loop)(mydebug t "ctxt:~a~%" (po-index)))
+		  ((funcall s0 "#," string when-flag)(mydebug t "#,:~a~%" (po-index)))
+		  ((eql nil string) (funcall s0 "" string when-blank-or-eof)(mydebug t "nil:~a~%" (po-index)))
+		  ((funcall s0 "#" string when-comment)(mydebug t "comment:~a~%" (po-index)))
+		  ((funcall s0 "" string when-blank-or-eof)(mydebug t "empty:~a~%" (po-index)))
+		  (t (error (format nil "unexpect:~a~%" string)))))))
     (po-index-save)
     (po-reset-index)
     (do ()
@@ -303,7 +315,7 @@
     ;; pre sql output
     (if pre-sql
 	(loop for i in (probe-list pre-sql)
-	     do (format out "~a~%" i)))
+	   do (format out "~a~%" i)))
     
     (if db-filepath 
 	(flet ((if-table-exists-rename (db-filepath table-name)
@@ -320,12 +332,12 @@
 			 ;; $dbh->do("alter table '${t1}_$j1' rename to '${t1}_$j2'");
 			 (format nil "alter table '~a' rename to '~:*~a_~a';" table-name (1- number-of-tables)))
 		       nil))))
-	       (loop for i in `(,po-table-name ,headinfo-table-name)
-		  for sql = (if-table-exists-rename db-filepath i)
-		  if sql 
-		  do (format out "~a~%" sql))))
+	  (loop for i in `(,po-table-name ,headinfo-table-name)
+	     for sql = (if-table-exists-rename db-filepath i)
+	     if sql 
+	     do (format out "~a~%" sql))))
 
-	  ;; $dbh->do("create table '$t1' (id integer,msgid text,msgstr text,msgctxt text,fuzzy bool,flag text,pof text)");
+    ;; $dbh->do("create table '$t1' (id integer,msgid text,msgstr text,msgctxt text,fuzzy bool,flag text,pof text)");
     ;; $dbh->do("create table '$t2' (pof text,lname text,lmail text,tname text,tmail text,charset text,pforms text)");
     (format out "create table '~a' (id integer,msgid text,msgstr text,msgctxt text,fuzzy bool,flag text,pof text);~%" po-table-name)
     (format out "create table '~a' (pof text,lname text,lmail text,tname text,tmail text,charset text,pforms text);~%" headinfo-table-name)
@@ -335,11 +347,11 @@
     ;; 	(setf po-files (list po-files)))
     (setf po-files (probe-list po-files))
     (loop for po in po-files
-	 for po-file-name = (namestring po)
-	 do (po-read po)
-	 do (format out "~a~%" (headinfo-sql headinfo-table-name po-file-name (po-get-headinfo)))
-	 do (loop for i in (po-sql po-table-name po-file-name (po-parse))
-	       do (format out "~a~%" i)))
+       for po-file-name = (namestring po)
+       do (po-read po)
+       do (format out "~a~%" (headinfo-sql headinfo-table-name po-file-name (po-get-headinfo)))
+       do (loop for i in (po-sql po-table-name po-file-name (po-parse))
+	     do (format out "~a~%" i)))
 
     ;; output index sql
     (if db-filepath
@@ -355,10 +367,10 @@
 		(com-with-sqlite3
 		 db-filepath
 		 (format nil "select count(name) from sqlite_master where name like '~a%';" po-table-name)))))
-	      ;; $dbh->do("create index '$i1' on '$t1' (id,msgid,msgstr,msgctxt,fuzzy,flag,pof)");
-	      ;; $dbh->do("create index '$i2' on '$t2' (pof,lname,lmail,tname,tmail,charset,pforms)");
-	      (format out "create index '~a_~a' on '~a' (id,msgid,msgstr,msgctxt,fuzzy,flag,pof);~%" index-of-po number-of-po po-table-name)
-	      (format out "create index '~a_~a' on '~a' (pof,lname,lmail,tname,tmail,charset,pforms);~%" index-of-headinfo number-of-headinfo headinfo-table-name))
+	  ;; $dbh->do("create index '$i1' on '$t1' (id,msgid,msgstr,msgctxt,fuzzy,flag,pof)");
+	  ;; $dbh->do("create index '$i2' on '$t2' (pof,lname,lmail,tname,tmail,charset,pforms)");
+	  (format out "create index '~a_~a' on '~a' (id,msgid,msgstr,msgctxt,fuzzy,flag,pof);~%" index-of-po number-of-po po-table-name)
+	  (format out "create index '~a_~a' on '~a' (pof,lname,lmail,tname,tmail,charset,pforms);~%" index-of-headinfo number-of-headinfo headinfo-table-name))
 	(let ((index-of-headinfo (concatenate-strings "i_" headinfo-table-name))
 	      (index-of-po (concatenate-strings "i_" po-table-name)))
 	  (format out "create index '~a' on '~a' (id,msgid,msgstr,msgctxt,fuzzy,flag,pof);~%" index-of-po po-table-name)
@@ -372,13 +384,13 @@
     (format out "commit;~%")))
 
 (defun max-string(s1 s2)
-	 (loop
-	    with b1 = (length s1)
-	    with b2 = (length s2)  
-	    for i from 0 upto b1
-	    if (or (>= i b2) (char/= (char s1 i)
-		       (char s2 i)))
-	    return (subseq s1 0 i)))
+  (loop
+     with b1 = (length s1)
+     with b2 = (length s2)  
+     for i from 0 upto b1
+     if (or (>= i b2) (char/= (char s1 i)
+			      (char s2 i)))
+     return (subseq s1 0 i)))
 
 (defun test ()
   (let* ((po-table "t_")
@@ -403,37 +415,59 @@
     (multiple-value-bind (db-file-path table-suffix output-file)
 	(values-list
 	 (loop for i in argv
-	    if (all-matches "\\.po$" i)
+	    if (scan "\\.po$" i)
 	    do (push i po-files)
 	    else
+	    unless (scan "\\.lisp$" i)
 	    collect i into opt
 	    finally (return opt)))
       (list (or db-file-path *default-db-file-path*)
 	    (or table-suffix *default-table-suffix*)
 	    (or output-file *default-sql*)
 	    po-files))))
-(defun main ()
+(defun main2 ()
+  ;; (format t "hello,world!~%")
   (destructuring-bind
 	(db-file-path table-suffix output-file po-files)
       (argv)
     (if (null po-files)
 	(format
 	 *standard-output*
-	 "Usage: ~a [db-file-path] [table-suffix] [sql-file] po-files~%~aReport po2db.lisp bugs to jeova.sanctus.unus~agmail.org~%"
-	#+sbcl
-	(car sb-ext:*posix-argv*)
-	#-sbcl
-	"lisp"
-	(with-output-to-string (out)
-	  (loop for (i j)in `(`,("db-file-path" ,*default-db-file-path*)
-				`,("table-suffix" ,*default-table-suffix*)
-				`,("sql-file" ,*default-sql*))
-	       do (format out "The default value of ~a is ~a~%" i j)))
-	"@")
+	 "Usage: ~a~a [dot-lisp-file [db-file-path [table-suffix [sql-file]]]] po-files~%~aReport po2db.lisp bugs to jeova.sanctus.unus~agmail.org~%Git: https://github.com/chenfengyuan/po2db~%"
+	 #+sbcl
+	 (car sb-ext:*posix-argv*)
+	 #-sbcl
+	 "lisp"
+	 *version-string*
+	 (with-output-to-string (out)
+	   (loop for (i j)in `(`,("db-file-path" ,*default-db-file-path*)
+				 `,("table-suffix" ,*default-table-suffix*)
+				 `,("sql-file" ,*default-sql*))
+	      do (format out "The default value of ~a is ~a~%" i j)))
+	 "@")
 	(let ((headinfo-table-name (concatenate-strings *default-headinfo-prefix* table-suffix))
 	      (po-table-name (concatenate-strings *default-table-prefix* table-suffix)))
 	  (po2sql po-files output-file headinfo-table-name po-table-name :db-filepath db-file-path)
 	  (com-with-sqlite3 db-file-path (concatenate-strings ".read " output-file))))))
+(defun main ()
+  (defun hot-update ()
+    (let ((first (cadr
+		  #+sbcl
+		  sb-ext:*posix-argv*))
+	  fasl)
+      (if (and first
+	       (scan "\\.lisp$" first)
+	       (> (file-write-date first) *version*))
+	  (progn
+	    (setf fasl (replace (copy-seq first) ".fasl" :start1 (- (length first) 5)))
+	    (if (probe-file fasl)
+		(delete-file fasl))
+	    (load first)
+	    (main2)
+	    (sb-ext:save-lisp-and-die "po2db" :toplevel #'cfy.po2db:main :executable t)
+	    )
+	  (main2))))
+  (hot-update))
 ;; compile as elf
 ;; (declaim (optimize (speed 3)(debug 0)(space 3)))
 ;; (load "/home/cfy/gits/po2db/po2db.lisp")
