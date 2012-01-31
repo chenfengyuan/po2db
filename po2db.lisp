8e7f728487e07003b5acdcc803a5c7b46256c8c6
Fengyuan Chen <cfy1990@gmail.com>
2012-01-31 16:17:06 +0800

    fix typo in email address

--- po2db.lisp
+++ po2db.lisp
@@ -1,4 +1,4 @@
-;; Copyright (C) 2011,2012 Chen Fengyuan (jeova.sanctus.unus+po2db (at) gmail.com)
+;; Copyright (C) 2011,2012 Chen Fengyuan (jeova.sanctus.unus+po2db (at) gmail.org)
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
